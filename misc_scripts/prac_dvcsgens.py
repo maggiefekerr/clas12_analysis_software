b6a2ab6e98bee50068b84f4173f6b07e75d01282
maggiefekerr <maggiekerr@Maggies-MacBook-Pro.local>
2025-06-18 13:07:09 -0400

    updated vgg python code

--- misc_scripts/prac_dvcsgens.py
+++ misc_scripts/prac_dvcsgens.py
@@ -6,18 +6,21 @@ os.environ['PATH'] = 'u/home/mkerr/dvcsgens/dvcsgen_print:' + os.environ.get('PA
 os.environ['CLASDVCS_PDF'] = '/u/home/mkerr/dvcsgens/dvcsgen_print'
 
 # returns polarized cross section for +'ve bea, polarization
-def vgg_model_xs_pos(xB, Q2, t_pos, phi_rad, beam_E=10.604, bh=3, gpd=101, globalfit=True):
+def vgg_model_xs_pos(x, Q2, t_pos, phi_rad, beam=10.604, bh=3, gpd=101, globalfit=True):
     # calls dvcsgen in print mode and returns positively polarized cross section
-    cmd = ['dvcsgen',
-            '--beam', f'{beam_E:.3f}',
-            '--x', str(xB), str(xB),
-            '--q2', str(Q2), str(Q2),
-            '--t', str(t_pos), str(t_pos),
-            '--phi', f'{phi_rad:.6f}',
-            '--bh', str(bh),
-            '--gpd', str(gpd),
-            '--ycol', '0.0001'
-          ]
+
+    cmd = [
+        'dvcsgen',
+        '--beam', f'{beam:.3f}',
+        '--x',    str(x), str(x),
+        '--q2',   str(Q2), str(Q2),
+        '--t',    str(t), str(t),
+        '--phi',  f'{phi_rad:.6f}',
+        '--bh',   str(bh),
+        '--gpd',  str(gpd),
+        '--ycol', '0.0001'
+    ]
+
     if globalfit:
         cmd.append('--globalfit')
     proc = subprocess.run(cmd,
@@ -34,19 +37,21 @@ def vgg_model_xs_pos(xB, Q2, t_pos, phi_rad, beam_E=10.604, bh=3, gpd=101, globa
 
 
 # returns polarized cross section for -'ve beam polarization
-def vgg_model_xs_neg(xB, Q2, t_pos, phi_rad, beam_E=10.604, bh=3, gpd=101, globalfit=True):
+def vgg_model_xs_neg(x, Q2, t_pos, phi_rad, beam=10.604, bh=3, gpd=101, globalfit=True):
     # calls dvcsgen in print mode and returns negatively polarized cross section
 
-    cmd = ['dvcsgen',
-            '--beam', f'{beam_E:.3f}',
-            '--x', str(xB), str(xB),
-            '--q2', str(Q2), str(Q2),
-            '--t', str(t_pos), str(t_pos),
-            '--phi', f'{phi_rad:.6f}',
-            '--bh', str(bh),
-            '--gpd', str(gpd),
-            '--ycol', '0.0001'
-          ]
+    cmd = [
+        'dvcsgen',
+        '--beam', f'{beam:.3f}',
+        '--x',    str(x), str(x),
+        '--q2',   str(Q2), str(Q2),
+        '--t',    str(t), str(t),
+        '--phi',  f'{phi_rad:.6f}',
+        '--bh',   str(bh),
+        '--gpd',  str(gpd),
+        '--ycol', '0.0001'
+    ]
+
     if globalfit:
         cmd.append('--globalfit')
     proc = subprocess.run(cmd,
