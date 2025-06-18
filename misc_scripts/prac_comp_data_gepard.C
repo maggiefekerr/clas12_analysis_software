693acb9f79991c0f6059992954d97d038fbe56db
maggiefekerr <maggiekerr@Maggies-MacBook-Pro.local>
2025-06-18 10:11:04 -0400

    made a new directory for misc scripts for easier pushing and pulling

---
+++ misc_scripts/prac_comp_data_gepard.C
@@ -0,0 +1,129 @@
+#include <TSystem.h>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// .x prac_comp_data_gepard.C("fall2018_out.root", 0.126, 1.759, -0.670, 10.604, 9, 0.89)
+// How I run this
+
+void prac_comp_data_gepard(TString file, Double_t xB, Double_t q2, Double_t t, Int_t numBins, Double_t E_beam=10.604, Double_t beamPol=0.89){ // lowercase q2 is just so ROOT doesn't crash out
+
+    // first making the initial asymmetry histogram plot
+
+    Double_t xB_min, xB_max, Q2_min, Q2_max, t_min, t_max, tpos, tpos_min, tpos_max;
+
+    // kinematic definitions & ranges
+    xB_min = xB - 0.02;
+    xB_max = xB + 0.02;
+    Q2_min = q2 - 0.2;
+    Q2_max = q2 + 0.2;
+    t_min = t - 0.1;
+    t_max = t + 0.1;
+    tpos = -1*t;
+    tpos_min = -1*t_max;
+    tpos_max = -1*t_min;
+
+    TFile *f = TFile::Open(file);
+    TH1F *hPhi_pos = new TH1F("hPhi_pos", "hPhi_pos", numBins, 0, 2*TMath::Pi());
+    TH1F *hPhi_neg = new TH1F("hPhi_neg", "hPhi_neg", numBins, 0, 2*TMath::Pi());
+
+    // reading through events and handling them
+    TTreeReader r("PhysicsEvents", f);
+    TTreeReaderValue<double> phi2(r, "phi2");
+    TTreeReaderValue<int> helicity(r, "helicity");
+    TTreeReaderValue<double> x(r, "x");
+    TTreeReaderValue<double> Q2(r, "Q2");
+    TTreeReaderValue<double> t1(r, "t1");
+
+    while (r.Next()) {
+        if ((xB_min < *x < xB_max) && (Q2_min < *Q2 < Q2_max) && (tpos_min < *t1*-1 < tpos_max)){
+            if (*helicity == +1) {
+                hPhi_pos->AddBinContent(hPhi_pos->FindBin(*phi2));
+            }
+            if (*helicity == -1) {
+                hPhi_neg->AddBinContent(hPhi_pos->FindBin(*phi2));
+            }
+        }
+    }
+    TH1F *hAdd = new TH1F("hAdd", "hAdd", numBins, 0, 2*TMath::Pi());
+    hAdd->Add(hPhi_pos, hPhi_neg);
+    TH1F *hSub = new TH1F("hSub", "hSub", numBins, 0, 2*TMath::Pi());
+    hSub->Add(hPhi_pos, hPhi_neg, 1, -1);
+    TH1F *hAsymData = new TH1F("hAsymData", "hAsymData", numBins, 0, 2*TMath::Pi());
+    hAsymData->Divide(hSub, hAdd);
+    hAsymData->Scale(1./beamPol);
+
+    // making model asymmetry, this was chatgpt cooking
+    Int_t numBinDiv = 10;
+    TH1F *hAsymKM15 = new TH1F("", "", numBins*numBinDiv, 0, 2*TMath::Pi());
+    for (int i=1; i<=numBins; i++) {
+        for (int j=0; j<numBinDiv; j++) {
+            std::ostringstream oss;
+            Double_t pos = hAsymData->GetBinCenter(i) - 0.5*hAsymData->GetBinWidth(i) + j*hAsymData->GetBinWidth(i)/numBinDiv;
+            oss << "python -u prac_gepard.py km15_model " << xB << " " << q2 << " " << tpos << " " << pos*TMath::RadToDeg() << " " << E_beam << " ALU";
+            std::string pythonRun = oss.str();
+            
+            FILE* pipe = gSystem->OpenPipe(pythonRun.c_str(), "r");
+
+            if (!pipe) {
+                std::cerr << "Failed to run Python script" << std::endl;
+                return;
+            }
+
+            char buffer[128];
+            std::string result;
+            while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+                result += buffer;
+            }
+            gSystem->ClosePipe(pipe);
+            Double_t outAsym = std::stod(result);
+            hAsymKM15->SetBinContent((i-1)*numBinDiv + (j+1), outAsym);
+        }
+    }
+
+    // plot and compare
+    TCanvas *c1 = new TCanvas("c1", "c1", 2000, 1500);
+    hAsymData->SetMarkerColor(4);
+    hAsymData->SetLineColor(4);
+    hAsymData->SetMarkerStyle(21);
+    hAsymData->SetStats(1);
+
+    hAsymKM15->SetMarkerColor(2);
+    hAsymKM15->SetLineColor(2);
+    hAsymKM15->SetMarkerStyle(21);
+    hAsymKM15->SetTitle("A_{LU} from Fall 2018 RG-A and KM15 Model");
+    hAsymKM15->GetXaxis()->SetTitle("#phi (rad)");
+    hAsymKM15->GetYaxis()->SetTitle("#frac{N^{+}-N^{-}}{N^{+}+N^{-}}");
+    hAsymKM15->SetStats(0);
+
+    for (Int_t i=1; i<=numBins; i++) {
+        hAsymData->SetBinError(i, 1/TMath::Sqrt((hAdd->GetBinContent(i))*beamPol));
+    }
+
+    TF1 *fit = new TF1("fit", "[0]*sin(x)/(1+[1]*cos(x))+[2]", 0, 2*TMath::Pi());
+    fit->SetParameter(0,1);
+    fit->SetParameter(1,1);
+    fit->SetParameter(2,0);
+    fit->SetLineColor(4);
+
+    hAsymKM15->Draw("P");
+    hAsymData->Draw("P sames");
+    hAsymData->Fit("fit");
+    gStyle->SetOptFit(0001);
+
+    TLegend *leg1 = new TLegend(0.1,0.1,0.2,0.3);
+    leg1->AddEntry(hAsymData,"data","ep");
+    leg1->AddEntry(hAsymKM15,"km15","p");
+    leg1->Draw();
+
+    TLatex *l = new TLatex();
+    l->SetTextSize(0.035);
+    l->DrawLatexNDC(0.625, 0.53, "([p0]*sin(x))/(1+[p1]*cos(x))+[p2]");
+    l->DrawLatexNDC(0.225, 0.250, Form("x_{B}: %.3f", xB));
+    l->DrawLatexNDC(0.225, 0.200, Form("Q^{2}: %.3f", q2));
+    l->DrawLatexNDC(0.225, 0.150, Form("-t: %.3f", tpos));
+
+    c1->Update();
+    c1->SaveAs("prac_comp-data-depard_comp.png");
+}
