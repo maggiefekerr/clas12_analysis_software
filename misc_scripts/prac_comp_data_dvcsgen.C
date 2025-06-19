5ebf5adc49dc5c02ba40db324c21bd57e1bcc2f4
maggiefekerr <maggiekerr@Maggies-MacBook-Pro.local>
2025-06-19 01:04:30 -0400

    updated vgg python code

--- misc_scripts/prac_comp_data_dvcsgen.C
+++ misc_scripts/prac_comp_data_dvcsgen.C
@@ -71,11 +71,9 @@ void prac_comp_data_dvcsgen(TString file, Double_t xB, Double_t q2, Double_t t, 
     
     for(int i=0; i <=(numBins*numBinDiv); i++) {
         if_vgg >> phiVal >> xsPosVal >> xsNegVal;
-        cout << phiVal << " " << hAsymVGG->GetBinCenter(i+1) << endl;
+        cout << phiVal*TMath::DegToRad() << " " << hAsymVGG->GetBinCenter(i+1) << endl;
         hAsymVGG->SetBinContent(i+1, (xsPosVal-xsNegVal)/(xsPosVal+xsNegVal));
     }
-
-    cout << phiVal << " " << xsPosVal << " " << xsNegVal;
     if_vgg.close();
     
     /*TFile *f_vgg = TFile::Open("vgg_xs_phi-xsPos-xsNeg.txt", "r");
