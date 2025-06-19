67b5127b8dddc52bb5b05f1da5cf1f0c27f6706e
maggiefekerr <maggiekerr@Maggies-MacBook-Pro.local>
2025-06-19 00:22:32 -0400

    updated vgg python code

--- misc_scripts/prac_dvcsgens.py
+++ misc_scripts/prac_dvcsgens.py
@@ -6,39 +6,40 @@ os.environ['PATH'] = '/u/home/mkerr/dvcsgens/dvcsgen_print:' + os.environ.get('P
 os.environ['CLASDVCS_PDF'] = '/u/home/mkerr/dvcsgens/dvcsgen_print'
 
 # returns polarized cross section for +'ve bea, polarization
-def vgg_model(xB, Q2, tpos, numBins, beamE=10.604, bh=3, gpd=101, globalfit=True):
+def vgg_model(xB, Q2, tpos, numBins, numDiv, beamE=10.604, bh=3, gpd=101, globalfit=True):
     # calls dvcsgen in print mode and writes a file containing the phi values, pos & neg cross sections
     phiList = []
     xsPosList = []
     xsNegList = []
 
     for i in range(numBins):
-        phi_deg = (((2.0*i)+1.0)/2.0)*(360.0/numBins)
-        cmd = [
-            'dvcsgen',
-            '--beam', f'{beamE:.3f}',
-            '--x',    str(xB), str(xB),
-            '--q2',   str(Q2), str(Q2),
-            '--t',    str(tpos), str(tpos),
-            '--phi',  f'{phi_deg:.6f}',
-            '--bh',   str(bh),
-            '--gpd',  str(gpd),
-            '--ycol', '0.0001'
-        ]
-        if globalfit:
-            cmd.append('--globalfit')
-        proc = subprocess.run(cmd,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            text=True)
-        if proc.returncode != 0:
-            raise RuntimeError(f"dvcsgen failed:\n{proc.stderr}")
-        lines = proc.stdout.splitlines()
-        numeric = [ln for ln in lines if ln.strip()]
-        
-        phiList.append(phi_deg)
-        xsPosList.append((float)(numeric[-2]))
-        xsNegList.append((float)(numeric[-3]))
+        for j in range(numDiv):
+            phi_deg = (i*360/numBins) + (360.0/numBins)*(j/numDiv)
+            cmd = [
+                'dvcsgen',
+                '--beam', f'{beamE:.3f}',
+                '--x',    str(xB), str(xB),
+                '--q2',   str(Q2), str(Q2),
+                '--t',    str(tpos), str(tpos),
+                '--phi',  f'{phi_deg:.6f}',
+                '--bh',   str(bh),
+                '--gpd',  str(gpd),
+                '--ycol', '0.0001'
+            ]
+            if globalfit:
+                cmd.append('--globalfit')
+            proc = subprocess.run(cmd,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                text=True)
+            if proc.returncode != 0:
+                raise RuntimeError(f"dvcsgen failed:\n{proc.stderr}")
+            lines = proc.stdout.splitlines()
+            numeric = [ln for ln in lines if ln.strip()]
+            
+            phiList.append(phi_deg)
+            xsPosList.append((float)(numeric[-2]))
+            xsNegList.append((float)(numeric[-3]))
 
     with open("vgg_xs_phi-xsPos-xsNeg.txt", "w") as f:
         for i in range(len(phiList)):
