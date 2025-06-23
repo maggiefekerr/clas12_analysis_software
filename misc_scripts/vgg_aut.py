c24871fc11a82ffb0589e16e1c1509f610b3be81
maggiefekerr <maggiekerr@dhcp-10-31-170-102.dyn.MIT.EDU>
2025-06-23 17:16:49 -0400

    added new scripts for model analysis

---
+++ misc_scripts/vgg_aut.py
@@ -0,0 +1,68 @@
+import sys
+sys.stdout.reconfigure(line_buffering=True)
+import os
+import subprocess
+import concurrent.futures
+import numpy as np
+
+# pointing at print-mode dvcsgen installation
+os.environ['PATH'] = '/u/home/mkerr/dvcsgens/dvcsgen_print:' + os.environ.get('PATH','')
+os.environ['CLASDVCS_PDF'] = '/u/home/mkerr/dvcsgens/dvcsgen_print'
+
+def vgg_aut(file, xB, Q2, tpos, numBins, beamE=10.604):
+    # calls dvcsgen in print mode and writes a file containing the phi values, pos & neg cross sections
+    inputPhiList = [20., 60., 100., 140., 180., 220., 260., 300., 340.]
+    phiList = []
+    xsPosList = []
+    xsNegList = []
+
+    for i in range(numBins):
+        phi_rad = (2.0*i +1.0)*0.5*(2*np.pi/numBins) - 0.5*2*np.pi/numBins + j*2*np.pi/(numDiv*numBins) + 2*np.pi/(2.0*numDiv*numBins)
+        cmd = [
+            'dvcsgen',
+            '--beam', f'{beamE:.3f}',
+            '--x',    str(xB), str(xB),
+            '--q2',   str(Q2), str(Q2),
+            '--t',    str(tpos), str(tpos),
+            '--phi',  f'{phi_rad:.6f}',
+            '--bh',   '3',
+            '--gpd',  '101',
+            '--ycol', '0.0001'
+        ]
+        cmd.append('--tpol')
+        cmd.append('--globalfit')
+        print(cmd)
+        proc = subprocess.run(cmd,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            text=True)
+        if proc.returncode != 0:
+            raise RuntimeError(f"dvcsgen failed:\n{proc.stderr}")
+        lines = proc.stdout.splitlines()
+        numeric = [ln for ln in lines if ln.strip()]
+                
+        phiList.append(phi_rad)
+        xsPosList.append((float)(numeric[-3]))
+        xsNegList.append((float)(numeric[-2]))    
+
+    with open(file, "w") as f:
+        for i in range(numBins):
+            if i != (len(numBins) - 1):
+                f.write(str(phiList[i]) + " " + str(xsPosList[i]) + " " + str(xsNegList[i]) + '\n')
+            else:
+                f.write(str(phiList[i]) + " " + str(xsPosList[i]) + " " + str(xsNegList[i]))
+    f.close()
+
+if __name__ == "__main__":
+   method = sys.argv[1]
+    a = sys.argv[2]
+    b = sys.argv[3]
+    c = sys.argv[4]
+    d = sys.argv[5]
+    e = sys.argv[6]
+    f = sys.argv[7]
+
+    if method == "vgg_aut":
+        km15_aut(str(a), float(b), float(c), float(d), int(e), float(f))
+    else:
+        print(":(")
