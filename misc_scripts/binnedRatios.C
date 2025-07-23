20d89c8b2eeef9007632ab574f40ef13b460e2fc
Maggie Kerr <maggiekerr@Maggies-MacBook-Pro.local>
2025-07-23 15:27:48 -0400

    added scripts to misc_scripts dir

--- misc_scripts/binnedRatios.C
+++ misc_scripts/binnedRatios.C
@@ -380,8 +380,8 @@ void plot_ratios_xs() {
         hRatio[i]->SetTitle(Form("#int_{0}^{2#pi}d#sigma^{#it{H}} / #int_{0}^{2#pi}d#sigma^{all}, -t = %.2f GeV^{2}", t[i]));
         hRatio[i]->GetYaxis()->SetTitle("Q^2 [GeV^{2}]");
         hRatio[i]->GetXaxis()->SetTitle("x_{B}");
-        hRatio[i]->GetZaxis()->SetTitle("#int_{0}^{2#pi}d#sigma^{#it{H}} / #int_{0}^{2#pi}d#sigma^{all}");
-        hRatio[i]->GetZaxis()->SetRangeUser(0.0, 2.5);
+        //hRatio[i]->GetZaxis()->SetTitle("#int_{0}^{2#pi}d#sigma^{#it{H}} / #int_{0}^{2#pi}d#sigma^{all}");
+        hRatio[i]->GetZaxis()->SetRangeUser(0.0, 2.0);
         hRatio[i]->SetStats(0);
         hRatio[i]->Draw("COLZ");
 
