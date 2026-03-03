d6e2b8d22d7d1f72abd7851960f3cf4387b28c70
Maggie Kerr <maggiekerr@Maggies-MacBook-Pro.local>
2026-03-03 00:09:02 -0500

    change

--- processing_classes/src/extended_kinematic_fitters/tcs_fitter.java
+++ processing_classes/src/extended_kinematic_fitters/tcs_fitter.java
@@ -192,7 +192,7 @@ public class tcs_fitter extends GenericKinematicFitter {
 
                     Particle elec = new Particle(pid, px, py, pz, vx, vy, vz_e);
                     physEvent.addParticle(elec);
-                    System.out.println("added electron");
+                    System.out.println(Double.toString(pz));
                 }
 
                 if (pid == -11) {
@@ -205,7 +205,7 @@ public class tcs_fitter extends GenericKinematicFitter {
                     pz = momentum[2];
                     Particle posi = new Particle(pid, px, py, pz, vx, vy, vz);
                     physEvent.addParticle(posi);
-                    System.out.println("added positron");
+                    System.out.println(Double.toString(pz));
                 }
 
                 //if (pid == 2212 && proton_test(particle_Index, pid, vz, vz_e, rec_Bank, cal_Bank,
@@ -222,7 +222,7 @@ public class tcs_fitter extends GenericKinematicFitter {
                     pz = momentum[2];
                     Particle prot = new Particle(pid, px, py, pz, vx, vy, vz);
                     physEvent.addParticle(prot);
-                    System.out.println("added proton");
+                    System.out.println(Double.toString(pz));
                 }
             }
 
