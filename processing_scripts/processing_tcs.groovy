5c757f69ab3b3599588e2ae151952c44d107e3d1
Maggie Kerr <maggiekerr@Maggies-MacBook-Pro.local>
2026-03-04 16:10:22 -0500

    rebuilding

--- processing_scripts/processing_tcs.groovy
+++ processing_scripts/processing_tcs.groovy
@@ -101,7 +101,7 @@ public static void main(String[] args) {
 	GenericKinematicFitter fitter = new tcs_fitter(10.6041);
 
     // set filter for final states
-	EventFilter filter = new EventFilter("11:-11:2212:X+:X-:Xn");  
+	EventFilter filter = new EventFilter("11:-11:2212");  
 
     // setup QA database
 	QADB qa = new QADB("latest");
