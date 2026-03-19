8c5241792f72ee641753f9b2fdd1fd910db0fb0b
Maggie Kerr <maggiekerr@Maggies-MacBook-Pro.local>
2026-03-19 10:28:12 -0400

    change

--- processing_scripts/processing_ntcs.groovy
+++ processing_scripts/processing_ntcs.groovy
@@ -254,7 +254,6 @@ public static void main(String[] args) {
 					// Append the line to the batchLines StringBuilder
 	                batchLines.append(line.toString());
 	                lineCount++; // Increment the line count
-					System.out.println(lineCount);
 
 	                // If the line count reaches 1000, write to the file and reset
 	                if (lineCount >= max_lines) {
