c62771fa8ad21ac8da262b8b353c533f091eedff
Maggie Kerr <mkerr284@mit.edu>
2026-04-23 15:38:38 -0400

    testing

--- processing_scripts/processing_two_particles_raw.groovy
+++ processing_scripts/processing_two_particles_raw.groovy
@@ -234,7 +234,7 @@ public static void main(String[] args) {
                         double isrTheta_deg = Math.toDegrees(isrTheta)
                         double isrPhi_deg   = Math.toDegrees(isrPhi)
 
-                        println(helicity)
+                        println(fiducial_status)
 
                         StringBuilder line = new StringBuilder()
                         line.append(fiducial_status).append(" ")
